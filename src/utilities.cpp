224dc91e22055a51e567986d27eb0917f641ca51
glasdam95 <glasdam95@gmail.com>
2021-02-19 11:25:30 +0100

    Code: Initial encryption

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -3,7 +3,7 @@
 
 void print_uint8(const uint8_t *input, int size) {
     for(int i = 0; i < size; i++){
-        printf("%d", input[i]);
+        printf("0x%02x ", input[i]);
         fflush(stdout);
     }
 }
