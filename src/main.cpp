33202b1e041e8568f6325135ec2e5f4bc2270122
glasdam95 <glasdam95@gmail.com>
2021-02-26 09:42:33 +0100

    Code: Setup main with structs

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,51 @@
 #include <acorn.h>
 
 #include <aes_gcm.h>
+#include <ascon.h>
 
 #define SIZE 16
 
 
+ struct encryption {
+    void (*acorn_encryption)(uint8_t *plaintext, uint8_t *ciphertext, uint8_t *tag, int size, uint8_t *key, uint8_t *iv, int cipherSize);
+    void (*aes_gcm_encryption)(uint8_t *plaintext, uint8_t *ciphertext, uint8_t *tag, int size, uint8_t *key, uint8_t *iv, int cipherSize);
+    void (*ascon_encryption)(uint8_t *plaintext, uint8_t *ciphertext, uint8_t *tag, int size, uint8_t *key, uint8_t *iv, int cipherSize);
+};
+
+struct decryption {
+    void (*acorn_decryption)(uint8_t *ciphertext, uint8_t *plaintext, uint8_t *tag, int size, uint8_t *key, uint8_t *iv, int cipherSize);
+    void (*aes_gcm_decryption)(uint8_t *ciphertext, uint8_t *plaintext, uint8_t *tag, int size, uint8_t *key, uint8_t *iv, int cipherSize);
+    void (*ascon_decryption)(uint8_t *ciphertext, uint8_t *plaintext, uint8_t *tag, int size, uint8_t *key, uint8_t *iv, int cipherSize);
+};
+
+struct cipherOperator {
+    struct encryption encryption;
+    struct decryption decryption;
+    uint8_t key[SIZE];
+};
+
 void setup() {
     Serial.begin(115200);
+
+    struct cipherOperator cipher ={
+        {
+            /** Defined encryption methods **/
+            acorn_encryption,
+            aes_gcm_encryption,
+            ascon_encryption
+        },{
+            /** Defined decryption methods **/
+            acorn_decryption,
+            aes_gcm_decryption,
+            ascon_decryption
+        },{
+        /** Key definition **/
+             0x00, 0x01, 0x02, 0x03, 0x04,
+             0x05, 0x06, 0x07, 0x08, 0x09,
+             0x0a, 0x0b, 0x0c, 0x0d, 0x0e,
+             0x0f
+        }
+    };
 
     /** Define initial text **/
     char input[] = "hej med dig min ven";
@@ -25,43 +64,24 @@ void setup() {
 
     /** Validation process **/
     uint8_t tag[SIZE];
-    uint8_t plaintextReceiver   [textSize];
-    uint8_t ciphertextReceiver  [textSize];
+    uint8_t plaintextReceiver[textSize];
+    uint8_t ciphertextReceiver[textSize];
 
     /** Key and IV initialization **/
-    uint8_t key[SIZE] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
+    //uint8_t key[SIZE] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
     uint8_t iv[SIZE] = {0x00, 0x03, 0x06, 0x09, 0x0c, 0x0f, 0x12, 0x15, 0x18, 0x1b, 0x1e, 0x21, 0x24, 0x27, 0x2a, 0x2d};
 
+
     /** Encryption **/
-    acorn_encryption(plaintext, ciphertextReceiver, tag, textSize, key, iv, SIZE);
+    cipher.encryption.ascon_encryption(plaintext, ciphertextReceiver, tag, textSize, cipher.key, iv, SIZE);
     print_uint8(ciphertextReceiver, textSize);
 
     /** Decryption **/
-    acorn_decryption(ciphertextReceiver, plaintextReceiver, tag, textSize, key, iv, SIZE);
+    cipher.decryption.ascon_decryption(ciphertextReceiver, plaintextReceiver, tag, textSize, cipher.key, iv, SIZE);
     print_uint8(plaintextReceiver, textSize);
+
 
     /** Convert received plaintext to readable char array **/
-    uint8ToChar(plaintextReceiver, backToText, textSize);
-    print_char(backToText, textSize);
-
-
-    /** Starts AES cryptographic algorithm **/
-    Serial.println("--------------------------------");
-    /** Reset all arrays **/
-    memset(tag, 0xBA, SIZE);
-    memset(plaintextReceiver, 0xBA, textSize);
-    memset(ciphertextReceiver, 0xBA, textSize);
-    memset(backToText, 0xBA, textSize);
-
-    /** AES Encryption **/
-    aes_gcm_encryption(plaintext, ciphertextReceiver, tag, textSize, key, iv,SIZE);
-    print_uint8(ciphertextReceiver, textSize);
-
-    /** AES Decryption **/
-    aes_gcm_decryption(ciphertextReceiver, plaintextReceiver, tag, textSize, key, iv, SIZE);
-    print_uint8(plaintextReceiver, textSize);
-
-    /** Convert to char array **/
     uint8ToChar(plaintextReceiver, backToText, textSize);
     print_char(backToText, textSize);
 }
