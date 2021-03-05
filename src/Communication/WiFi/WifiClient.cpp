916322411f9e5531674199e3e61871b0eaac55b1
glasdam95 <glasdam95@gmail.com>
2021-03-05 15:56:07 +0100

    WiFi: Add basic wifi functionality

---
+++ src/Communication/WiFi/WifiClient.cpp
@@ -0,0 +1,50 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "ESP32-Access-Point";
+const char* password = "123456789";
+
+const char aesResource[] = "http://192.168.4.1/aes";
+
+String httpGETRequest(const char* serverName) {
+    HTTPClient http;
+
+    http.begin(serverName);
+    int httpResponseCode = http.GET();
+
+    String payload = "--";
+
+    if (httpResponseCode > 0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        payload = http.getString();
+    }
+    else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+    }
+    http.end();
+
+    return payload;
+}
+
+void setup(){
+    Serial.begin(115200);
+
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting");
+    while(WiFiClass::status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    String aescode = httpGETRequest(aesResource);
+    Serial.println(aescode);
+
+}
+
+void loop(){
+}
