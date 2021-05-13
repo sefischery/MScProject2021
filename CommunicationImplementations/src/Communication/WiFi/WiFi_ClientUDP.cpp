319aeaf20b7e2ef8224ed20fb8907f9a1a1c678a
SebastianFischer164158 <s164158@student.dtu.dk>
2021-05-13 11:18:05 +0200

    WiFi_ClientUDP: now works with UDP

--- CommunicationImplementations/src/Communication/WiFi/WiFi_ClientUDP.cpp
+++ CommunicationImplementations/src/Communication/WiFi/WiFi_ClientUDP.cpp
@@ -9,25 +9,32 @@ void Connect_To_AP(const char *SSID){
     Serial.println("Connecting to the Access Point: ");
     Serial.println(SSID);
 
-    // Connect to WiFi
     WiFi.begin(SSID);
-    while (WiFiClass::status() != WL_CONNECTED)
-    {
+    while (WiFiClass::status() != WL_CONNECTED) {
         delay(500);
+        Serial.println("Connecting to WiFi..");
     }
 
-    Serial.println("Connected to WiFi - IP Address: ");
-    Serial.print(WiFi.localIP());
+    Serial.println("Connected to WiFi");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("MAC Address: ");
     Serial.println(WiFi.macAddress());
 }
 
 void setup() {
     Serial.begin(115200);
-    UDP.begin(UDP_PORT);
     Connect_To_AP(AP_SSID);
+    UDP.begin(UDP_PORT);
+
 }
 void loop(){
     delay(10000);
-    Build_And_Send_UDP_Packet(UDP, "test", Server_IP, UDP_PORT);
+    Serial.println();
+    const char *msg = "This is an unecrypted UDP message sent via WiFi";
+    String output_format = "Sending UDP packet to ["+Server_IP.toString()+":" + UDP_PORT + "]" + " - Sent message: ";
+    Serial.println(output_format);
+    Serial.println(msg);
+    Build_And_Send_UDP_Packet(UDP, msg, Server_IP, UDP_PORT);
 }
 
